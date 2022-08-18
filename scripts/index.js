e5c727ba2ed2ca3c0ea73d8ccc7cc576d03f7d73
tiigroid <tiigroid@yandex.ru>
2022-08-18 21:37:35 +0300

    fix: исправить замечания из ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,49 +1,27 @@
-let popup = document.querySelector('.popup');
-let popupButtonClose = document.querySelector('.popup__button-close');
-let popupForm = document.querySelector('.popup__container');
-let popupName = document.querySelector('.popup__name');
-let popupStatus = document.querySelector('.popup__status');
-let profileName = document.querySelector('.profile__name');
-let profileStatus = document.querySelector('.profile__status');
-let profileButtonEdit = document.querySelector('.profile__button-edit');
+const popup = document.querySelector('.popup');
+const popupForm = document.querySelector('.popup__container');
+const popupName = document.querySelector('.popup__text-input_type_name');
+const popupStatus = document.querySelector('.popup__text-input_type_status');
+const popupButtonClose = document.querySelector('.popup__button-close');
+const profileName = document.querySelector('.profile__name');
+const profileStatus = document.querySelector('.profile__status');
+const profileButtonEdit = document.querySelector('.profile__button-edit');
 
-function popupToggle () {
-  popup.classList.toggle('popup_opened');
-  popupName.value = '';
-  popupStatus.value = '';
-  popupName.placeholder = profileName.textContent;
-  popupStatus.placeholder = profileStatus.textContent;
-  popupName.classList.remove('red-placeholder');
-  popupStatus.classList.remove('red-placeholder');
+function popupOpen () {
+  popup.classList.add('popup_opened');
+  popupName.value = profileName.textContent;
+  popupStatus.value = profileStatus.textContent;
+};
+function popupClose () {
+  popup.classList.remove('popup_opened');
 };
 function submitPopupForm (evt) {
   evt.preventDefault();
-  if (popupName.value.length === 0 || popupStatus.value.length === 0) {
-    popupName.placeholder = 'Обязательно к заполнению :)';
-    popupStatus.placeholder = 'Обязательно к заполнению :)';
-    popupName.classList.add('red-placeholder');
-    popupStatus.classList.add('red-placeholder');
-    } else {
     profileName.textContent = popupName.value;
     profileStatus.textContent = popupStatus.value;
-    popupToggle();
-    };
+    popupClose();
 };
 
-profileButtonEdit.addEventListener('click', popupToggle);
-popupButtonClose.addEventListener('click', popupToggle);
+profileButtonEdit.addEventListener('click', popupOpen);
+popupButtonClose.addEventListener('click', popupClose);
 popupForm.addEventListener('submit', submitPopupForm);
-popupName.addEventListener('focus', function clearPlaceholder () {
-  popupName.placeholder = '';
-});
-popupStatus.addEventListener('focus', function clearPlaceholder () {
-  popupStatus.placeholder = '';
-});
-popupName.addEventListener('blur', function returnNamePlaceholder () {
-  popupName.placeholder = profileName.textContent;
-  popupName.classList.remove('red-placeholder');
-});
-popupStatus.addEventListener('blur', function returnStatusPlaceholder () {
-  popupStatus.placeholder = profileStatus.textContent;
-  popupStatus.classList.remove('red-placeholder');
-});
