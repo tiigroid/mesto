50c3f77b587b04b3b12f41092dc272851d571f60
tiigroid <tiigroid@yandex.ru>
2022-09-04 22:07:00 +0300

    feat: добавлять стартовые карточки через js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,46 @@
+const initialCards = [
+  {
+    name: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1488278905738-514111aa236c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Новая Зеландия',
+    link: 'https://images.unsplash.com/photo-1504064860048-974c8788c612?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  },
+  {
+    name: 'Морро Бэй',
+    link: 'https://images.unsplash.com/photo-1597839271116-399cd739ddb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Кейп-Бретон',
+    link: 'https://images.unsplash.com/photo-1540913570178-23a49e4e8616?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+  },
+  {
+    name: 'Сан-Диего',
+    link: 'https://images.unsplash.com/photo-1609994819063-e8de7339880b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  },
+  {
+    name: 'Залив Файерс',
+    link: 'https://images.unsplash.com/photo-1597762453091-4467f4b15761?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+  }
+];
+
+const galleryCardTemplate = document.querySelector('#gallery__card').content;
+const gallery = document.querySelector('.gallery');
+
+initialCards.forEach(function (item) {
+  const galleryCard = galleryCardTemplate.querySelector('.gallery__card').cloneNode(true);
+  galleryCard.querySelector('.gallery__image').src = item.link;
+  galleryCard.querySelector('.gallery__image').alt = galleryCard.querySelector('.gallery__title').textContent = item.name;
+  gallery.prepend(galleryCard);
+});
+
+
+
+
+
+
+
 const popup = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__container');
 const popupName = document.querySelector('.popup__text-input_type_name');
