0727add9fcc9d392b422a40d0ffc6065ee53c3f5
tiigroid <tiigroid@yandex.ru>
2022-09-07 02:33:18 +0300

    bugfix and refactor: исправить ошибки, оптимизировать код

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    place: 'Санкт-Петербург',
+    link: 'https://images.unsplash.com/photo-1488278905738-514111aa236c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    place: 'Новая Зеландия',
+    link: 'https://images.unsplash.com/photo-1504064860048-974c8788c612?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  },
+  {
+    place: 'Морро Бэй',
+    link: 'https://images.unsplash.com/photo-1597839271116-399cd739ddb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    place: 'Кейп-Бретон',
+    link: 'https://images.unsplash.com/photo-1540913570178-23a49e4e8616?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    place: 'Сан-Диего',
+    link: 'https://images.unsplash.com/photo-1609994819063-e8de7339880b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+  },
+  {
+    place: 'Залив Файерс',
+    link: 'https://images.unsplash.com/photo-1597762453091-4467f4b15761?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,11 @@
-const initialCards = [
-  {
-    place: 'Санкт-Петербург',
-    link: 'https://images.unsplash.com/photo-1488278905738-514111aa236c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    place: 'Новая Зеландия',
-    link: 'https://images.unsplash.com/photo-1504064860048-974c8788c612?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-  },
-  {
-    place: 'Морро Бэй',
-    link: 'https://images.unsplash.com/photo-1597839271116-399cd739ddb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    place: 'Кейп-Бретон',
-    link: 'https://images.unsplash.com/photo-1540913570178-23a49e4e8616?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
-  },
-  {
-    place: 'Сан-Диего',
-    link: 'https://images.unsplash.com/photo-1609994819063-e8de7339880b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-  },
-  {
-    place: 'Залив Файерс',
-    link: 'https://images.unsplash.com/photo-1597762453091-4467f4b15761?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
-  },
-];
-
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const profileButtonEdit = document.querySelector('.profile__button-edit');
 const profileButtonAdd = document.querySelector('.profile__button-add');
+
+const popupsWithForms = document.querySelectorAll('.popup_type_form');
+const popupEdit = popupsWithForms[0];
+const popupAdd = popupsWithForms[1];
 
 const popupFormEdit = document.querySelector('.popup__form_type_edit');
 const popupFormEditInputName = document.querySelector('.popup__input_type_name');
@@ -38,7 +15,7 @@ const popupFormAdd = document.querySelector('.popup__form_type_add');
 const popupFormAddInputPlace = document.querySelector('.popup__input_type_place');
 const popupFormAddInputLink = document.querySelector('.popup__input_type_link');
 
-const popupFullview = document.querySelector('.popup__type_fullview');
+const popupFullview = document.querySelector('.popup_type_fullview');
 const popupFullviewImage = document.querySelector('.popup__image');
 const popupFullviewCaption = document.querySelector('.popup__caption');
 
@@ -56,14 +33,17 @@ function makeCard(place, link) {
   galleryCard.querySelector('.gallery__title').textContent = place;
   galleryCard.querySelector('.gallery__image').alt = place;
   galleryCard.querySelector('.gallery__image').src = link;
+  const buttonDelete = galleryCard.querySelector('.gallery__button-delete');
+  const buttonLike = galleryCard.querySelector('.gallery__button-like');
+  const galleryImage = galleryCard.querySelector('.gallery__image');
+  buttonDelete.addEventListener('click', deleteCard);
+  buttonLike.addEventListener('click', toggleLike);
+  galleryImage.addEventListener('click', openPopupFullview);
   return galleryCard;
 }
 
-function addCard(result) {
-  gallery.prepend(result);
-  renderImageListeners();
-  renderLikeListeners();
-  renderDeleteListeners();
+function addCard(cardToAdd) {
+  gallery.prepend(cardToAdd);
 }
 
 function openPopupFullview(evt) {
@@ -73,11 +53,11 @@ function openPopupFullview(evt) {
 }
 
 function openPopup(popup) {
-  popup.closest('.popup').classList.add('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
 function closePopup(popup) {
-  popup.closest('.popup').classList.remove('popup_opened');
+  popup.classList.remove('popup_opened');
 }
 
 function toggleLike(evt) {
@@ -98,52 +78,31 @@ function fillPopupFormAdd() {
   popupFormAddInputLink.value = '';
 }
 
-function renderImageListeners() {
-  galleryImages = document.querySelectorAll('.gallery__image');
-  galleryImages.forEach((image) => {
-    image.addEventListener('click', openPopupFullview);
-  });
-}
-
-function renderLikeListeners() {
-  buttonsLike = document.querySelectorAll('.button_type_like');
-  buttonsLike.forEach((button) => {
-    button.addEventListener('click', toggleLike);
-  });
-}
-
-function renderDeleteListeners() {
-  buttonsDelete = document.querySelectorAll('.button_type_delete');
-  buttonsDelete.forEach((button) => {
-    button.addEventListener('click', deleteCard);
-  });
-}
-
 profileButtonEdit.addEventListener('click', () => {
   fillPopupFormEdit();
-  openPopup(popupFormEdit);
+  openPopup(popupEdit);
 });
 
 profileButtonAdd.addEventListener('click', () => {
   fillPopupFormAdd();
-  openPopup(popupFormAdd);
+  openPopup(popupAdd);
 });
 
 popupFormEdit.addEventListener('submit', (evt) => {
   evt.preventDefault();
   profileName.textContent = popupFormEditInputName.value;
   profileStatus.textContent = popupFormEditInputStatus.value;
-  closePopup(popupFormEdit);
+  closePopup(popupEdit);
 });
 
 popupFormAdd.addEventListener('submit', (evt) => {
   evt.preventDefault();
   addCard(makeCard(popupFormAddInputPlace.value, popupFormAddInputLink.value));
-  closePopup(popupFormAdd);
+  closePopup(popupAdd);
 });
 
 buttonsClose.forEach((button) => {
   button.addEventListener('click', (evt) => {
-    closePopup(evt.target);
+    closePopup(evt.target.parentElement.parentElement);
   });
 });
