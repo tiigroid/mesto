f343682eeeabb613f87ec7cb5d720b952c8e7f09
tiigroid <tiigroid@yandex.ru>
2022-09-06 15:17:20 +0300

    wip, backup

--- scripts/index.js
+++ scripts/index.js
@@ -1,119 +1,146 @@
 const initialCards = [
   {
-    name: 'Санкт-Петербург',
+    place: 'Санкт-Петербург',
     link: 'https://images.unsplash.com/photo-1488278905738-514111aa236c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   },
   {
-    name: 'Новая Зеландия',
+    place: 'Новая Зеландия',
     link: 'https://images.unsplash.com/photo-1504064860048-974c8788c612?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
   },
   {
-    name: 'Морро Бэй',
+    place: 'Морро Бэй',
     link: 'https://images.unsplash.com/photo-1597839271116-399cd739ddb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   },
   {
-    name: 'Кейп-Бретон',
+    place: 'Кейп-Бретон',
     link: 'https://images.unsplash.com/photo-1540913570178-23a49e4e8616?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
   },
   {
-    name: 'Сан-Диего',
+    place: 'Сан-Диего',
     link: 'https://images.unsplash.com/photo-1609994819063-e8de7339880b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
   },
   {
-    name: 'Залив Файерс',
+    place: 'Залив Файерс',
     link: 'https://images.unsplash.com/photo-1597762453091-4467f4b15761?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
   }
 ];
-const gallery = document.querySelector('.gallery');
-const galleryCardTemplate = document.querySelector('#gallery__card').content;
 
-initialCards.forEach( (item) => {
-  const galleryCard = galleryCardTemplate.querySelector('.gallery__card').cloneNode(true);
-  galleryCard.querySelector('.gallery__image').src = item.link;
-  galleryCard.querySelector('.gallery__image').alt = galleryCard.querySelector('.gallery__title').textContent = item.name;
-  gallery.prepend(galleryCard);
-});
-
-const page = document.querySelector('.page');
-const popupTemplate = document.querySelector('#popup').content;
 const profileName = document.querySelector('.profile__name');
 const profileStatus = document.querySelector('.profile__status');
 const profileButtonEdit = document.querySelector('.profile__button-edit');
 const profileButtonAdd = document.querySelector('.profile__button-add');
 
-function createPopupEdit() {
-  const popup = popupTemplate.querySelector('.popup').cloneNode(true);
-  popup.querySelector('.popup__title').textContent = 'Редактировать профиль';
-  popup.querySelector('.popup__button-save').textContent = 'Сохранить';
-  const popupInputs = popup.querySelectorAll('.popup__input');
-  popupInputs[0].value = profileName.textContent;
-  popupInputs[1].value = profileStatus.textContent;
-  page.append(popup);
-    popup.querySelector('.popup__container').addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    profileName.textContent = popupInputs[0].value;
-    profileStatus.textContent = popupInputs[1].value;
-    popup.remove();
+const popupFormEdit = document.querySelector('.popup__form_type_edit');
+const popupFormEditInputName = document.querySelector('.popup__input_type_name');
+const popupFormEditInputStatus = document.querySelector('.popup__input_type_status');
+
+const popupFormAdd = document.querySelector('.popup__form_type_add');
+const popupFormAddInputPlace = document.querySelector('.popup__input_type_place');
+const popupFormAddInputLink = document.querySelector('.popup__input_type_link');
+
+const popupFullview = document.querySelector('.popup__type_fullview');
+const popupFullviewImage = document.querySelector('.popup__image');
+const popupFullviewCaption = document.querySelector('.popup__caption');
+
+const gallery = document.querySelector('.gallery');
+const galleryCardTemplate = document.querySelector('#gallery__card').content;
+
+const buttonsClose = Array.from(document.querySelectorAll('.button_type_close'));
+
+let galleryImages = Array.from(document.querySelectorAll('.gallery__image'));
+let buttonsLike = Array.from(document.querySelectorAll('.button_type_like'));
+let buttonsDelete = Array.from(document.querySelectorAll('.button_type_delete'));
+
+profileButtonEdit.addEventListener('click', openPopupProfileEdit);
+profileButtonAdd.addEventListener('click', openPopupProfileAdd);
+popupFormEdit.addEventListener('submit', submitFormProfileEdit);
+popupFormAdd.addEventListener('submit', submitFormProfileAdd);
+
+buttonsClose.forEach( (button) => {
+  button.addEventListener('click', (evt) => {
+    closePopup(evt.target)
   });
-  buttonCloseListener();
-}
+});
 
-function createPopupAdd() {
-  const popup = popupTemplate.querySelector('.popup').cloneNode(true);
-  popup.querySelector('.popup__title').textContent = 'Новое место';
-  popup.querySelector('.popup__button-save').textContent = 'Создать';
-  const popupInputs = popup.querySelectorAll('.popup__input');
-  popupInputs[0].placeholder = 'Название';
-  popupInputs[1].placeholder = 'Ссылка на картинку';
-  page.append(popup);
-  popup.querySelector('.popup__container').addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    const galleryCard = galleryCardTemplate.querySelector('.gallery__card').cloneNode(true);
-    galleryCard.querySelector('.gallery__image').src = popupInputs[0].value;
-    galleryCard.querySelector('.gallery__image').alt = galleryCard.querySelector('.gallery__title').textContent = popupInputs[1].value;
-    gallery.prepend(galleryCard);
-    popup.remove();
-    galleryButtonDeleteListener();
-    galleryButtonLikeListener();
+initialCards.forEach((initialCard) => {
+  makeCard(initialCard.place, initialCard.link);
+});
+
+function renderImageListeners() {
+  galleryImages = Array.from(document.querySelectorAll('.gallery__image'));
+  galleryImages.forEach( (image) => {
+    image.addEventListener('click', openPopupFullview)
   });
-  buttonCloseListener();
 }
 
-function galleryButtonDeleteListener() {
-  const galleryButtonDelete = Array.from(document.querySelectorAll('.gallery__button-delete'));
-  galleryButtonDelete.forEach( (item) => item.addEventListener('click', (evt) => {
-    evt.target.closest('.gallery__card').remove();
-    }));
+function renderLikeListeners() {
+  buttonsLike = Array.from(document.querySelectorAll('.button_type_like'));
+  buttonsLike.forEach( (button) => {
+    button.addEventListener('click', toggleLike)
+  });
 }
 
-function galleryButtonLikeListener() {
-  const galleryButtonLike = Array.from(document.querySelectorAll('.gallery__button-like'));
-  galleryButtonLike.forEach( (item) => item.addEventListener('click', (evt) => {
-    evt.target.classList.add('button_type_liked');
-  }));
+function renderDeleteListeners() {
+  buttonsDelete = Array.from(document.querySelectorAll('.button_type_delete'));
+  buttonsDelete.forEach( (button) => {
+    button.addEventListener('click', deleteCard)
+  });
 }
 
-function buttonCloseListener() {
-  const buttonClose = Array.from(document.querySelectorAll('.button_type_close'));
-  buttonClose.forEach( (item) => item.addEventListener('click', (evt) => {
-    evt.target.parentElement.parentElement.remove();
-  }));
+function makeCard(place, link) {
+  const galleryCard = galleryCardTemplate.querySelector('.gallery__card').cloneNode(true);
+  galleryCard.querySelector('.gallery__title').textContent = place;
+  galleryCard.querySelector('.gallery__image').alt = place;
+  galleryCard.querySelector('.gallery__image').src = link;
+  gallery.prepend(galleryCard);
+  renderImageListeners();
+  renderLikeListeners();
+  renderDeleteListeners();
 }
 
-function galleryImageListener() {
-  const galleryImage = Array.from(document.querySelectorAll('.gallery__image'));
-  galleryImage.forEach( (item) => item.addEventListener('click', (evt) => {
-    const fullviewTemplate = document.querySelector('#fullview').content;
-    const fullview = fullviewTemplate.querySelector('.fullview').cloneNode(true);
-    page.append(fullview);
-    fullview.querySelector('.fullview__image').src = item.src;
-    fullview.querySelector('.fullview__caption').textContent = fullview.querySelector('.fullview__image').alt = item.alt;
-    buttonCloseListener();
-  }));
+function openPopupProfileEdit() {
+  popupFormEditInputName.value = profileName.textContent;
+  popupFormEditInputStatus.value = profileStatus.textContent;
+  popupFormEdit.parentElement.classList.add('popup_opened');
 }
 
-galleryButtonDeleteListener();
-galleryButtonLikeListener();
-galleryImageListener();
-profileButtonEdit.addEventListener('click', createPopupEdit);
-profileButtonAdd.addEventListener('click', createPopupAdd);
+function openPopupProfileAdd() {
+  popupFormAddInputPlace.value = '';
+  popupFormAddInputLink.value = '';
+  popupFormAdd.parentElement.classList.add('popup_opened');
+}
+
+function submitFormProfileEdit(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupFormEditInputName.value;
+  profileStatus.textContent = popupFormEditInputStatus.value;
+  closePopup(popupFormEdit);
+}
+
+function submitFormProfileAdd(evt) {
+  evt.preventDefault();
+  makeCard(popupFormAddInputPlace.value, popupFormAddInputLink.value);
+  closePopup(popupFormAdd);
+}
+
+function openPopupFullview(evt) {
+  popupFullviewImage.src = evt.target.src;
+  popupFullviewCaption.textContent = evt.target.alt;
+  openPopup(popupFullview);
+}
+
+function openPopup(node) {
+  node.closest('.popup').classList.add('popup_opened');
+}
+
+function closePopup(node) {
+  node.closest('.popup').classList.remove('popup_opened');
+}
+
+function toggleLike(evt) {
+  evt.target.classList.toggle('button_type_liked');
+}
+
+function deleteCard(evt) {
+  evt.target.closest('.gallery__card').remove();
+}
